070a7493d3fbe5bd41063d7cb85efad1451aa74a
Hugo <hugoalvesx96@gmail.com>
2022-01-16 20:22:52 -0300

    primeiro commit do site

---
+++ js/modules/menu-mobile.js
@@ -0,0 +1,24 @@
+import outsideClick from './outside.js';
+
+export default function initMenuMobile(){
+
+}
+
+const menuButton = document.querySelector('[data-menu="button"]');
+
+const menuList = document.querySelector('[data-menu="list"]');
+const eventos = ['click', 'touchstart'];
+
+function openMenu(event) {
+  menuList.classList.add('active');
+  menuButton.classList.add('active');
+  outsideClick(menuList, ['click', 'touchstart'], () => {
+    
+    menuList.classList.remove('active');
+    menuButton.classList.remove('active');
+  });
+}
+
+eventos.forEach((ecento) => {
+menuButton.addEventListener('click', openMenu);
+})
